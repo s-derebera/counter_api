9b92e57fe368da696ae0aea1540ca3e181eeb899
Светлана Деребера <svetlanaderebera@Svetlanas-MacBook-Air.local>
2023-01-19 19:45:17 -0500

    serving first version of counter

---
+++ hello.py
@@ -0,0 +1,21 @@
+from flask import Flask
+from flask import json
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello_world():
+    return "<p>Hello, World!</p>"
+
+
+@app.route("/counter.json")
+def counter():
+    data =  {"counter": 1}
+
+    response = app.response_class(
+        response=json.dumps(data),
+        status=200,
+        mimetype='application/json'
+    )
+    
+    return response
