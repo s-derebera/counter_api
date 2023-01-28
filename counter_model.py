57bb65e28662614926cb3c7692790d080646ba96
Светлана Деребера <svetlanaderebera@Svetlanas-MacBook-Air.local>
2023-01-27 21:35:25 -0500

    Creating a decrease method

--- counter_model.py
+++ counter_model.py
@@ -10,3 +10,19 @@ class CounterModel:
             json.dump(data, f)
         
         return data
+
+    def decrease(self):
+        f = open('counter.json')
+        data = json.load(f)
+
+        with open('counter.json', "w") as f:
+            if data['counter'] > 0:
+                data['counter'] -= 1
+            else:
+                data['counter'] = 0
+            json.dump(data, f)
+
+        return data 
+
+if __name__ == "__main__":
+    c = CounterModel()
